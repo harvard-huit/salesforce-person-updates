651fd47669cbd274e0b414f7bdf55551624a2749
JaZahn <jcleveng@fas.harvard.edu>
2023-05-15 20:30:08 -0400

    adding an appconfig class in common to get dynamo values

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from common import isTaskRunning, logger, stack
+from common import isTaskRunning, logger, stack, AppConfig
 import pds
 from salesforce import HarvardSalesforce
 from transformer import SalesforceTransformer
@@ -159,126 +159,10 @@ class SalesforcePersonUpdates:
 
 sfpu = SalesforcePersonUpdates()
 # sfpu.update_single_person("80719647")
-sfpu.full_people_data_load()
+# sfpu.full_people_data_load()
 
 
-# hsf = HarvardSalesforce(
-#     domain = os.getenv('SF_DOMAIN'),
-#     username = os.getenv('SF_USERNAME'),
-#     password = os.getenv('SF_PASSWORD'),
-#     token = os.getenv('SF_SECURITY_TOKEN'),
-# )
+app_config = AppConfig("huit-full-sandbox", "aais-services-salesforce-person-updates-dev")
 
-
-
-# hsf.pushBulk("Contact", [{
-#   'Birthdate': '1980-11-17',
-#   'Email': 'null@harvard.edu',
-#   'FirstName': 'Test',
-#   'HUDA__hud_ADID__c': 'test1234',
-#   'HUDA__hud_BIRTH_DT__c': '1980-11-17',
-#   'HUDA__hud_DECEASED_FLAG__c': 'N',
-#   'HUDA__hud_EFFDT__c': '2007-12-21T02:32:17',
-#   'HUDA__hud_EFF_STATUS__c': 'A',
-#   'HUDA__hud_EPPN__c': '2940935f3b990175',
-#   'HUDA__hud_GENDER__c': 'M',
-#   'HUDA__hud_INTERNAL_ID__c': 'e9f928bc98814859a3595494e368bbcd',
-#   'HUDA__hud_MULE_UNIQUE_PERSON_KEY__c': '2940935f3b990175',
-#   'HUDA__hud_UUID__c': 'e9f928bc98814859a3595494e368bbcd',
-#   'LastName': 'Clevenger'
-# }])
-
-
-# NOTE: branches are linked to the contact record via the eppn, not the contact relationship
-#       that's weird, right?
-# hsf.pushBulk("HUDA__hud_Name__c", [{
-#   'HUDA__EFFDT__c': '2018-10-23T02:32:49',
-#   'HUDA__EFF_STATUS__c': 'A',
-#   'HUDA__INTERNAL_ID__c': 'JaZahn',
-#   'HUDA__MULE_UNIQUE_PERSON_KEY__c': 'acd3d0471e7ed076',
-#   'HUDA__NAME_FIRST__c': 'JaZahn',
-#   'HUDA__NAME_LAST__c': 'Clevenger',
-#   'HUDA__NAME_MIDDLE__c': None,
-#   'HUDA__NAME_PREFIX__c': None,
-#   'HUDA__NAME_SUFFIX__c': None,
-#   'HUDA__NAME_TYPE__c': 'LISTING',
-#   'HUDA__PERSON_NAMES_KEY__c': '3701467',
-#   'HUDA__UNIV_ID__c': 'JaZahn',
-#   'HUDA__UPDATE_DT__c': '2018-10-23T02:32:49',
-#   'HUDA__UPDATE_SOURCE__c': None,
-#   'Id': 'aDmD40000001DrJKAU',
-#   'HUDA__Name_Contact__c': '00336000010CxNQAA0'
-# }])
-
-# #N/A doesn't seem to work for date/datetimes
-# data = [{'HUDA__hud_ACADEMIC_PRIME_ROLE_INDICATOR__c': 'F',
-#   'HUDA__hud_DEPT_ID__c': '104130',
-#   'HUDA__hud_EFFDT__c': '2022-11-05T02:33:11',
-#   'HUDA__hud_EFF_STATUS__c': 'A',
-#   'HUDA__hud_POI_COMMENTS__c': None,
-#   'HUDA__hud_EMP_ADDR_PS_LOCATION_CD__c': 'H86079',
-#   'HUDA__hud_EMP_APPOINT_END_DT__c': '2023-09-30T00:00:00',
-#   'HUDA__hud_EMP_DEPT_ENTRY_DT__c': '2022-11-01T00:00:00',
-#   'HUDA__hud_EMP_EMPLOYMENT_STATUS__c': 'A',
-#   'HUDA__hud_EMP_EMPL_CLASS__c': 'Y',
-#   'HUDA__hud_EMP_FACULTY_CD__c': 'FAS',
-#   'HUDA__hud_EMP_FULLTIME_FLAG__c': 'T',
-#   'HUDA__hud_EMP_HIRE_DT__c': '2022-11-01T00:00:00',
-#   'HUDA__hud_EMP_JOBCODE__c': '069591',
-#   'HUDA__hud_EMP_MAJ_AFFILIATION_CD__c': 'FAS^MA',
-#   'HUDA__hud_EMP_MAJ_AFFILIATION_DESC__c': 'Fac Arts & Scis Major Affil',
-#   'HUDA__hud_EMP_PAID_FLAG__c': 'T',
-#   'HUDA__hud_EMP_PAYGROUP__c': 'MIP',
-#   'HUDA__hud_EMP_REHIRE_DT__c': '2022-11-01T00:00:00',
-#   'HUDA__hud_EMP_SUB_AFFILIATION_CD__c': 'FAS_SCI^SA',
-#   'HUDA__hud_EMP_SUB_AFFILIATION_DESC__c': 'FAS^Sciences',
-#   'HUDA__hud_EMP_TERMINATION_DT__c': None,
-#   'HUDA__hud_EMP_UNION_CD__c': '00',
-#   'HUDA__hud_PERSON_ROLES_KEY__c': '6002319',
-#   'HUDA__hud_PRIME_ROLE_INDICATOR__c': 'T',
-#   'HUDA__hud_PRIVACY_VALUE__c': '5',
-#   'HUDA__hud_ROLE_END_DT__c': None,
-#   'HUDA__hud_ROLE_ID__c': '0',
-#   'HUDA__hud_ROLE_SOURCE__c': 'PS',
-#   'HUDA__hud_ROLE_START_DT__c': '2022-11-01T00:00:00',
-#   'HUDA__hud_ROLE_TITLE__c': 'Post-Doctoral Fellow in FAS Informatics',
-#   'HUDA__hud_ROLE_TYPE_CD__c': 'EMPLOYEE',
-#   'HUDA__hud_SUPERVISOR_ID__c': '60751145',
-#   'HUDA__hud_UPDATE_DT__c': '2022-11-05T02:33:11',
-#   'Id': 'a0B1R000014NwyXUAS',
-#   'hed__Contact__c': '0031R00002rj19OQAQ'}]
-# otherdata = [
-#  {'HUDA__hud_EFFDT__c': '2022-10-28T16:43:58',
-#   'HUDA__hud_EFF_STATUS__c': 'A',
-#   'HUDA__hud_PERSON_ROLES_KEY__c': '5998552',
-#   'HUDA__hud_POI_COMMENTS__c': '#N/A',
-#   'HUDA__hud_POI_COMPANY__c': '#N/A',
-#   'HUDA__hud_POI_FACULTY_CD__c': 'FAS',
-#   'HUDA__hud_POI_SHORT_DESC_LINE1__c': '#N/A',
-#   'HUDA__hud_POI_SHORT_DESC_LINE2__c': '#N/A',
-#   'HUDA__hud_PRIME_ROLE_INDICATOR__c': 'F',
-#   'HUDA__hud_PRIVACY_VALUE__c': '1',
-#   'HUDA__hud_ROLE_END_DT__c': '2022-11-08',
-#   'HUDA__hud_ROLE_ID__c': '1',
-#   'HUDA__hud_ROLE_SOURCE__c': 'PORTAL',
-#   'HUDA__hud_ROLE_START_DT__c': '2022-11-01T00:00:00',
-#   'HUDA__hud_ROLE_TITLE__c': 'Postdoctoral Fellow',
-#   'HUDA__hud_ROLE_TYPE_CD__c': 'INCEMPL',
-#   'HUDA__hud_UPDATE_DT__c': '2022-10-28T16:43:58',
-#   'Id': 'a0B1R000014NwYLUA0',
-#   'hed__Contact__c': '0031R00002rj19OQAQ'}]
-
-
-# hsf.pushBulk("hed__Affiliation__c", data)
-
-
-# response = hsf.sf.Contact.metadata()
-# response = hsf.getTypeMap(objects=config.keys())
-# logger.info(json.dumps(response))
-# logger.info(pformat(json.dumps(response)))
-
-# contact_results = hsf.getContactIds(id_type='HUDA__hud_UNIV_ID__c', ids=['91156571'])
-# logger.info(pformat(contact_results))
-
-# departments = Departments(apikey=os.getenv("DEPT_APIKEY")).departments
-# logger.info(departments)
+logger.info(f"id: {app_config.id}")
+logger.info(f"config: {app_config.config}")
--- src/common.py
+++ src/common.py
@@ -10,6 +10,11 @@ import boto3
 from dotenv import load_dotenv
 load_dotenv() 
 
+
+#============================================================================================
+# logging
+#============================================================================================
+
 import logging
 stack = os.getenv('STACK') or 'developer'
 debug = os.getenv('DEBUG') == "True" or False
@@ -27,28 +32,82 @@ logger = logging.getLogger(__name__)
 
 
 #============================================================================================
-# Utility Functions
+# AppConfig
 #============================================================================================
+class AppConfig():
+    def __init__(self, id, table_name):
+
+        self.id = id
+        self.name = id
+        self.table_name = table_name
+        self.salesforce_username = None
+        self.pds_query = None
+        self.config = None
+
+        self.salesforce_password = None
+        self.salesforce_token = None
+        self.salesforce_client_key = None
+        self.salesforce_client_secret = None
+
+        self.pds_apikey = None
+        self.dept_apikey = None
+
+
+        self.get_config_values()
+
+
+    def get_config_values(self):
+        # table_name = "aais-services-salesforce-person-updates-dev"
+
+        try:
+            dynamo = boto3.client('dynamodb')
+            response = dynamo.get_item(
+                Key={
+                    'id': {'S': self.id},
+                    'name': {'S': self.name}
+                },
+                TableName=self.table_name
+            )
+            if 'Item' in response:
+                self.salesforce_username = response.get('Item').get('salesforce_username').get('S')
+                self.pds_query = json.loads(response.get('Item').get('pds_query').get('S'))
+                self.config = json.loads(response.get('Item').get('transformation_config').get('S'))
+
+                salesforce_password_arn = response.get('Item').get('salesforce_username').get('S')
+                salesforce_token_arn = response.get('Item').get('salesforce_username').get('S')
+                salesforce_client_key_arn = response.get('Item').get('salesforce_username').get('S')
+                salesforce_client_secret_arn = response.get('Item').get('salesforce_username').get('S')
+
+                pds_apikey_arn = None
+                dept_apikey_arn = None
+
+            
+            else:
+                raise Exception(f"Error: unable to retrieve table values for table {self.table_name}: {response}")
+        except Exception as e:
+            logger.error(f"Error: failure to get configuration for id:{self.id} on table: {self.table_name}")
+            raise e
+        
+        try:
+        
+            self.salesforce_password = None
+            self.salesforce_token = None
+            self.salesforce_client_key = None
+            self.salesforce_client_secret = None
+
+            self.pds_apikey = None
+            self.dept_apikey = None
+
+        except Exception as e:
+            logger.error(f"Error: failure to get secrets manager values")    
+            raise e
+        
+    def get_secret(self, arn):
+        pass
 
 #============================================================================================
-# AWS Functions
+# Other
 #============================================================================================
-
-#============================================================================================
-# get data from DynamoDB
-#============================================================================================
-def getConfig(nameStr):
-    dynamo = boto3.client('dynamodb')
-    retValue = dynamo.get_item(
-        Key={
-            'name': {'S': nameStr}
-        },
-        TableName="salesforce-person-updates-config",
-    )    
-    if retValue.get('Item') == None:
-        return False
-    else:
-        return retValue.get('Item').get('flag_value').get('BOOL')
 
 # returns True when the person-updates task is running in the configured 
 # TODO: this needs to be tested for fargates
