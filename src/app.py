9771184a33dfc56d9bb6c51082706995de935c37
JaZahn <jcleveng@fas.harvard.edu>
2023-11-13 12:06:42 -0500

    limiting to a single thread

--- src/app.py
+++ src/app.py
@@ -391,7 +391,7 @@ class SalesforcePersonUpdates:
                     thread.start()
                     self.batch_threads.append(thread)
 
-                    while len(self.batch_threads) >= 2:
+                    while len(self.batch_threads) >= 1:
                         time.sleep(10)
                         for thread in self.batch_threads.copy():
                             # logger.info(f"{len(self.batch_threads)} unresolved threads")
