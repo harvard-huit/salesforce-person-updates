716dbfd7319dad0631160f322c3cd5c7489efc36
JaZahn <jcleveng@fas.harvard.edu>
2024-01-17 16:16:24 -0500

    typo

--- src/salesforce.py
+++ src/salesforce.py
@@ -140,7 +140,7 @@ class HarvardSalesforce:
                                 dupe_data_batch.append(errored_data)
 
                         # if it's invalid and the message is about an external id not existing, we want to ignore it
-                        if response['errors'][0]['statusCode']] == 'INVALID_FIELD':
+                        if response['errors'][0]['statusCode'] == 'INVALID_FIELD':
                             if response['errors'][0]['message'].startswith("Foreign key external ID"): 
                                 # we want to ignore this error as it happens if the Contact didn't make it
                                 logger.debug(f"Warning: INVALID_FIELD (external id): {response['errors'][0]['message']}")
