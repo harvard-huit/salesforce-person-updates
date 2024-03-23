af79edf88dd927d56fbfeae776c9ae0cd3ba2fad
JaZahn <jcleveng@fas.harvard.edu>
2024-03-23 17:07:11 -0400

    simplifying the code

--- src/person_reference.py
+++ src/person_reference.py
@@ -15,7 +15,7 @@ class PersonReference():
         elif environment == "stage":
             self.host="go.stage.apis.huit.harvard.edu"
         elif environment == "testing":
-            self.host="go.testing.apis.huit.harvard.edu"
+            self.host="not_real_url.com"
         else:
             raise ValueError(f"Error: unknown environment ({environment}). Possible values are defaulting to prod")
 
@@ -25,24 +25,47 @@ class PersonReference():
             "x-api-key": apikey
         }
 
+        if environment != "testing":
+            try:
+                self.healthCheck()
+            except Exception as e:
+                logger.error(f"Error: Unable to connect to Person Reference API {e}")
+                raise e
+            
+        # set up urls
+        self.health_url = f"https://{ self.host }/ats/person/reference/v1/monitor/health"
+
+        self.schools_url = f"https://{ self.host }/ats/person/reference/v1/studentSchool"
+        self.units_url = f"https://{ self.host }/ats/person/reference/v1/academicUnit"
+        self.departments_url = f"https://{ self.host }/ats/person/reference/v1/department"
+        self.sub_affiliations_url = f"https://{ self.host }/ats/person/reference/v1/subAffiliation"
+        self.major_affiliations_url = f"https://{ self.host }/ats/person/reference/v1/majorAffiliation"
+
     def healthCheck(self):
-        health_url = f"https://{ self.host }/ats/person/reference/v1/health"
-        try:
-            response = requests.get(health_url, headers=self.headers)
-            if(response.status_code != 200):
-                raise Exception(f"Error: Failure to connect to Reference API ({response.status_code})")
-
-            return response.text
-
-        except Exception as e:
-            logger.error(f"Failure to connect to Reference API: {e}")
-            raise e
-
+        return self.getResultsText(self.health_url)
+    
     def getSchools(self):
-        school_url = f"https://{ self.host }/ats/person/reference/v1/studentSchool"
+        return self.getResultsJson(self.schools_url)
 
+    def getUnits(self):
+        return self.getResultsJson(self.units_url) 
+
+    def getDepartments(self):
+        return self.getResultsJson(self.departments_url)
+
+    def getSubAffiliations(self):
+        return self.getResultsJson(self.sub_affiliations_url)
+
+    def getMajorAffiliations(self):
+        return self.getResultsJson(self.major_affiliations_url)
+    
+    def getMajAffiliations(self):
+        logger.warning("Warning: why are you using this? Please use getMajorAffiliations instead. getMajAffiliations will not be removed ever, I just want to shame you for not using the right method.")
+        return self.getResults(self.major_affiliations_url)
+
+    def getResultsJson(self, url):
         try:
-            response = requests.get(school_url, headers=self.headers)
+            response = requests.get(url, headers=self.headers)
             if(response.status_code != 200):
                 raise Exception("Error: failure with response from Reference API")
             
@@ -55,3 +78,14 @@ class PersonReference():
             logger.error(f"Error: {e}")
             raise e
         
+    def getResultsText(self, url):
+        try:
+            response = requests.get(url, headers=self.headers)
+            if(response.status_code != 200):
+                raise Exception("Error: failure with response from Reference API")
+            
+            return response.text
+
+        except Exception as e:
+            logger.error(f"Error: {e}")
+            raise e
--- src/tests/person_reference_test.py
+++ src/tests/person_reference_test.py
@@ -34,38 +34,71 @@ class PersonReferenceTest(unittest.TestCase):
         }
 
     @mock.patch('requests.get')
-    def test_get_health_check(self, mock_get):
+    def test_get_results_text(self, mock_get):
         mock_response = mock.MagicMock()
         mock_response.status_code = 200
         mock_response.text = "OK"
         mock_get.return_value = mock_response
-        expected_url = f"https://{self.person_reference.host}/ats/person/reference/v1/health"
 
-        # Call the healthCheck method
-        result = self.person_reference.healthCheck()
+        # Call the getResultsText method
+        result = self.person_reference.getResultsText(self.person_reference.health_url)
 
         # Assert the response
         self.assertEqual(result, "OK")
-        mock_get.assert_called_once_with(expected_url, headers=self.person_reference.headers)
-
+        mock_get.assert_called_once_with(self.person_reference.health_url, headers=self.person_reference.headers)
 
     @mock.patch('requests.get')
-    def test_get_schools(self, mock_get):
+    def test_get_results_text_failure(self, mock_get):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 500
+        mock_response.text = "Internal Server Error"
+        mock_get.return_value = mock_response
+
+        # Call the getResultsText method
+        with self.assertRaises(Exception) as e:
+            self.person_reference.getResultsText(self.person_reference.health_url)
+
+        # Assert the response
+        self.assertEqual(str(e.exception), "Error: failure with response from Reference API")
+
+    @mock.patch('requests.get')
+    def test_get_results_json(self, mock_get):
         mock_response = mock.MagicMock()
         mock_response.status_code = 200
         mock_response.json.return_value = self.fake_schools
-        mock_response.text = json.dumps(self.fake_schools)
         mock_get.return_value = mock_response
-        expected_url = f"https://{self.person_reference.host}/ats/person/reference/v1/studentSchool"
+
+        # Call the getResultsJson method
+        result = self.person_reference.getResultsJson(self.person_reference.schools_url)
+
+        # Assert the response
+        self.assertEqual(result, self.fake_schools)
+        mock_get.assert_called_once_with(self.person_reference.schools_url, headers=self.person_reference.headers)
+
+    @mock.patch('requests.get')
+    def test_get_results_json_failure(self, mock_get):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 500
+        mock_response.text = "Internal Server Error"
+        mock_get.return_value = mock_response
+
+        # Call the getResultsJson method
+        with self.assertRaises(Exception) as e:
+            self.person_reference.getResultsJson(self.person_reference.schools_url)
+
+        # Assert the response
+        self.assertEqual(str(e.exception), "Error: failure with response from Reference API")
+
+    @mock.patch('person_reference.PersonReference.getResultsJson')
+    def test_get_schools(self, mock_get):
 
         # Call the getSchools method
         result = self.person_reference.getSchools()
 
-        # Assert the response
-        self.assertEqual(result, self.fake_schools)
-        mock_get.assert_called_once_with(expected_url, headers=self.person_reference.headers)
-
+        # Assert the getResultsJson method was called
+        mock_get.assert_called_once_with(self.person_reference.schools_url)
     
+
 
 
 
