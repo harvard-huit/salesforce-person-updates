4467b51341dc7e528afee9948ae0a84e18d52fd2
JaZahn <jcleveng@fas.harvard.edu>
2024-05-31 14:47:26 -0400

    fixing uninitialized reference

--- src/app.py
+++ src/app.py
@@ -263,14 +263,19 @@ try:
     else: 
         logger.warning(f"App triggered without a valid action: {action}, please see documentation for more information.")
     output = "Success Apparent"
+    
 except Exception as e:
     action = os.getenv("action", None)
     salesforce_id = os.getenv("SALESFORCE_INSTANCE_ID", None)
     logger.error(f"Salesforce instance: {salesforce_id}, action: {action}: {e}")
     output = f"ERROR: {e}"
     raise e
+
 finally:
     if not stack == "developer":
         setTaskRunning(sfpu.app_config, False)
+
+        action = os.getenv("action", None)
+        salesforce_id = os.getenv("SALESFORCE_INSTANCE_ID", None)
         sfpu.app_config.stop_task_with_reason(f"Salesforce instance: {salesforce_id}: Action: {action} completed. {output}")
 
