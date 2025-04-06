0ef8495932e952f4b795da6931a575a9ff00dce9
JaZahn Clevenger <jcleveng@fas.harvard.edu>
2025-04-06 17:43:50 -0400

    removing test stuff

--- src/app.py
+++ src/app.py
@@ -273,10 +273,6 @@ try:
         logger.info(f"duplicate-check action finished")
     elif action == "test":
         logger.info(f"test action called")
-
-        sfpu.cleanup_updateds(object_name='Contact')
-        sfpu.cleanup_updateds(object_name='Affiliation__c')
-
         logger.info(f"test action finished")
     else: 
         logger.warning(f"App triggered without a valid action: {action}, please see documentation for more information.")
